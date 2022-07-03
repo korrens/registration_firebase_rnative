038fa324fff0115687e3772d367b787a6e27c67f
reiniskom <reinis.komuls@gmail.com>
2022-07-03 15:55:02 +0300

    DONE

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {};
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
